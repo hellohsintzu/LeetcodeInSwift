bead796ee370bdf893b2db32976d649846e4bf28
GordonFeng <hellogordon.f@gmail.com>
2022-06-26 14:34:06 +0800

    Array: Find Numbers with Even Number of Digits

---
+++ LeetcodeInSwiftTests/Array/FindNumbersWithEvenNumberOfDigits.swift
@@ -0,0 +1,50 @@
+//
+//  FindNumbersWithEvenNumberOfDigits.swift
+//  LeetcodeInSwiftTests
+//
+//  Created by Gordon Feng on 22/6/2022.
+//  https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/
+
+import XCTest
+
+class FindNumbersWithEvenNumberOfDigits: XCTestCase {
+
+    /// Given an array nums of integers, return how many of them contain an even number of digits.
+    ///
+    /// Input: nums = [12,345,2,6,7896]
+    ///
+    /// Output: 2
+    ///
+    /// Explanation:
+    ///
+    ///     12 contains 2 digits (even number of digits).
+    ///     345 contains 3 digits (odd number of digits).
+    ///     2 contains 1 digit (odd number of digits).
+    ///     6 contains 1 digit (odd number of digits).
+    ///     7896 contains 4 digits (even number of digits).
+    ///     Therefore only 12 and 7896 contain an even number of digits.
+
+    ///  Constraints:
+    ///  - 1 <= nums.length <= 500
+    ///  - 1 <= nums[i] <= 105
+    func findNumbers(_ nums: [Int]) -> Int {
+        var result = 0
+        for num in nums {
+            result += String(num).count % 2 == 0 ? 1 : 0
+        }
+        return result
+    }
+
+    func test_findNumbers() {
+        check(input: [12,345,2,6,7896], expect: 2)
+        check(input: [555,901,482,1771], expect: 1)
+        check(input: [], expect: 0)
+        check(input: [1], expect: 0)
+        check(input: [11], expect: 1)
+    }
+
+    func check(input: [Int], expect: Int) {
+        let result = findNumbers(input)
+        XCTAssertEqual(result, expect)
+    }
+}
