57525fa2ca215e82196865aaca67d41d02c30f1a
GordonFeng <hellogordon.f@gmail.com>
2022-07-09 15:58:58 +0800

    String: Implement strStr() - with a better solution.

--- LeetcodeInSwiftTests/String/Easy/ImplementStrStr.swift
+++ LeetcodeInSwiftTests/String/Easy/ImplementStrStr.swift
@@ -16,28 +16,36 @@ class ImplementStrStr: XCTestCase {
     /// interview.
     /// - For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
     func strStr(_ haystack: String, _ needle: String) -> Int {
-        guard !needle.isEmpty else { return 0 }
-        var haystacks = Array(haystack)
+        let hl = haystack.count
+        let nl = needle.count
+        //If needle is empty or needle == haystack, than return 0.
+        guard !needle.isEmpty && needle != haystack else { return 0 }
 
-        for h in 0..<haystack.count {
-            if String(haystacks).hasPrefix(needle) { return h }
-            else {
-                haystacks.removeFirst()
-            }
+        //if haystack is smaller then needle, return -1.
+        guard hl >= nl else { return -1 }
+
+        var checkPoint = haystack.startIndex    //every check start from this index
+
+        for i in 0...(hl-nl) {
+            //find the last index of every check
+            let lastPoint = haystack.index(checkPoint, offsetBy: nl)
+            //check the range of start to end is equal to needle.
+            if haystack[checkPoint..<lastPoint] == needle { return i }
+            //if no match, move the check point forward.
+            checkPoint = haystack.index(checkPoint, offsetBy: 1)
         }
         return -1
     }
 
-    func test() {
-        check(input: "mississippi", needle: "issip", expect: 4)
-    }
     /// Constraints:
     ///  - 1 <= haystack.length, needle.length <= 104
     ///  - haystack and needle consist of only lowercase English characters.
     func test_strStr() {
         check(input: "hello", needle: "ll", expect: 2)
         check(input: "aaaaa", needle: "bba", expect: -1)
+        check(input: "issip", needle: "issip", expect: 0)
         check(input: "mississippi", needle: "issip", expect: 4)
+        check(input: "abc", needle: "c", expect: 2)
     }
 
     func check(input: String, needle: String, expect: Int) {
